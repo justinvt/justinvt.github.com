3ba6f636617156380b0c9faee01b3b0d26a4c6ca
justin thibault <jvthibault@gmail.com>
2016-05-12 21:35:43 -0400

    Add files via upload

---
+++ js/projects_data.js
@@ -0,0 +1,32 @@
+var projects = {
+	tweetyjobs: {
+		title: "TweetyJobs",
+		images:[
+			"01_homepage",
+			"02_employer_signup",
+			"03_employer_settings"
+		],
+		description : ""
+	},
+	sellersdoor: {
+		title: "Sellers Door",
+		images:[
+		],
+		description : ""
+	},
+	scenechat: {
+		title: "SceneChat",
+		images:[
+
+		],
+		description : ""
+	},
+	comeover: {
+		title: "ComeOver",
+		images:[
+
+		],
+		description : ""
+	},
+
+}
