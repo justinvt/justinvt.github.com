1ad5e9eeeb93c6959f3d678b243b048c319074f5
justin thibault <jvthibault@gmail.com>
2016-05-12 22:13:44 -0400

    Update projects_data.js

--- js/projects_data.js
+++ js/projects_data.js
@@ -4,9 +4,36 @@ var projects = {
 		images:[
 			"01_homepage",
 			"02_employer_signup",
-			"03_employer_settings"
+			"03_employer_settings",
+			"04_employer_companyprofileEDIT",
+			"05_employer_postajobstandard",
+			"06_employer_postajobtweetit",
+			"07_employer_mypostedjobs",
+			"08_jobseeker_signup",
+			"09_jobseeker_settings",
+			"10_jobseeker_careerprofileEDIT",
+			"11_jobseeker_careerprofile",
+			"12_jobseeker_careerprofile_sharetweet",
+			"13_jobseeker_careerprofile_shareemail",
+			"14_jobseeker_careerprofile_sharesms",
+			"15_jobseeker_mainsearch",
+			"16_jobseeker_sharejobtweet_retweet",
+			"17_jobseeker_sharejobtweet_directmessage",
+			"18_jobseeker_sharejobtweet_@reply",
+			"19_jobseeker_sharejobtweet_email",
+			"20_jobseeker_sharejobtweet_sms",
+			"21_jobseeker_saveagent",
+			"22_jobseeker_agenthistory",
+			"23_jobseeker_editagent",
+			"24_jobseeker_companyprofile",
+			"25_jobseeker_jobtweetdetail",
+			"26_jobseeker_3rdpartyjob",
+			"27_jobseeker_leavejobdetail",
+			"28_jobseeker_interestedjobs",
+			"29_jobseeker_savedjobs",
+			"30_informational"
 		],
-		description : ""
+		description : "Here are some screenshots of tweety jobs"
 	},
 	sellersdoor: {
 		title: "Sellers Door",
